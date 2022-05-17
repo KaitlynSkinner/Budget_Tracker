972d4a2df9e8dacad0b1fb99e102ae11cd77f8c1
Kaitlyn Skinner <kskinnerr@hotmail.com>
2022-05-17 16:54:45 -0400

    updated service worker

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,7 +1,7 @@
 // create variable to hold db connection
 let db;
 // establish a connection to IndexedDB database called 'budget_db' and set it to version 1
-const request = indexedDB.open('budget_db', 1);
+const request = indexedDB.open('budget_tracker_db', 1);
 
 // this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
 request.onupgradeneeded = function(event) {
